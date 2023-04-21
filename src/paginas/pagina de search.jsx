dbb02a02daf566dca4dc40e72b15bab04255abc6
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-21 19:19:46 -0300

    Add files via upload
    adicionado pagina q exibe resultado da barra de busca

---
+++ src/paginas/pagina de search.jsx
@@ -0,0 +1,16 @@
+import Cabecalho from "../components/cabecalho"
+import Rodape from "../components/rodape"
+import Search from "../components/search"
+
+function PaginaSearch() {
+
+    return (
+        <div>
+            <Cabecalho />
+            <Search />
+            <Rodape />
+        </div>
+    )
+}
+
+export default PaginaSearch
