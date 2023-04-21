b47852a6dc761b1505570251ed9362a6abb0bc42
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-21 15:33:36 -0300

    Add files via upload
    adicionado o componente de introducao

--- src/paginas/pagina inicial.jsx
+++ src/paginas/pagina inicial.jsx
@@ -1,12 +1,14 @@
 import Cabecalho from "../components/cabecalho"
 import Rodape from "../components/rodape"
 import Demonstracao from "../components/Demonstracao"
+import Introducao from "../components/introducao"
 
 function PaginaInicial() {
 
     return (
         <div>
             <Cabecalho />
+            <Introducao />
             <Demonstracao />
             <Rodape />
         </div>
