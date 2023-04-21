9ed062c4f18252c199699810dddd068f169ee2df
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-21 17:21:26 -0300

    Add files via upload
    agora a pagina inicial cria demonstracoes de codigo usando forEach() de acordo com o numero de elemento do array

--- src/components/Demonstracao.jsx
+++ src/components/Demonstracao.jsx
@@ -6,61 +6,73 @@ import url from './url.js'
 function Demonstracao() {
     
     // const [id, SetId] = useState('')
-    const id = 1    // AKI VAI PEGAR O VALOR DO ID Q DEVE ESTAR NO INPUT
+    // const id = 1    // AKI VAI PEGAR O VALOR DO ID Q DEVE ESTAR NO INPUT
 
-    const [descricao, SetDescricao] = useState('')
-    const [tecHtml, SetTecHtml] = useState('')
-    const [tecCss, SetTecCss] = useState('')
-    const [tecJavascript, SetTecJavascript] = useState('')
+    // const [descricao, SetDescricao] = useState('')
+    // const [tecHtml, SetTecHtml] = useState('')
+    // const [tecCss, SetTecCss] = useState('')
+    // const [tecJavascript, SetTecJavascript] = useState('')
     
-    const [linkVerCompleto, setLinkVerCompleto] = useState('')
-    const [linkVerCodigo, setLinkVerCodigo] = useState('')
-    const [linkIframe, setLinkIframe] = useState('')
+    // const [linkVerCompleto, setLinkVerCompleto] = useState('')
+    // const [linkVerCodigo, setLinkVerCodigo] = useState('')
+    // const [linkIframe, setLinkIframe] = useState('')
+
+    // useEffect(() => {
+    //     api.get(`${url}/codigo?id=${id}`)
+    //     .then(res => {
+    //         SetDescricao(res.data.descricao)
+    //         SetTecHtml(res.data.techtml)
+    //         SetTecCss(res.data.teccss)
+    //         SetTecJavascript(res.data.tecjavascript)
+
+
+    //         setLinkVerCompleto(`/template?id=${id}`)
+    //         setLinkVerCodigo(`/codigo?id=${id}`)
+    //         setLinkIframe(`${url}/template?id=${id}`)
+    //     })
+    //     .catch(erro => alert(erro))
+    // }, [])
+
 
     useEffect(() => {
-        api.get(`${url}/codigo?id=${id}`)
+        api.get(`${url}/codigos`)
         .then(res => {
-            SetDescricao(res.data.descricao)
-            SetTecHtml(res.data.techtml)
-            SetTecCss(res.data.teccss)
-            SetTecJavascript(res.data.tecjavascript)
 
-            // document.getElementById('linkTemplate').href = `/template?id=${res.data.id}`
-            // document.getElementById('linkCodigo').href = `/codigo?id=${res.data.id}`
+            res.data.forEach((codigo) => {
+                document.getElementById('demonstracoes').innerHTML += 
+                `
+                <hr/>
+                <div class="demonstracao">
+                    <div class="informacoes">
+                        <p>${codigo.descricao}</p>
+                        <details>
+                            <summary>Tecnologias usadas</summary>
+                            <ol>
+                                <li id="tecHtml">HTML5: ${codigo.techtml}</li>
+                                <li id="tecCss">CSS3: ${codigo.teccss}</li>
+                                <li id="tecJavascript">JavaScript: ${codigo.tecjavascript}</li>
+                            </ol>
+                        </details>
+                        <div class="link">
+                            <a href="/template?id=${codigo.id}" target="_blank">Ver completo</a>
+                            <a href="/codigo?id=${codigo.id}">Ver código</a>
+                        </div>
+                    </div>
+    
+                    <iframe src="${url}/template?id=${codigo.id}"></iframe>
+                </div>
+                `
+            })
 
-            setLinkVerCompleto(`/template?id=${id}`)
-            setLinkVerCodigo(`/codigo?id=${id}`)
-            setLinkIframe(`${url}/template?id=${id}`)
         })
         .catch(erro => alert(erro))
     }, [])
 
+
     return (
-        <main>
+        <main id="demonstracoes">
 
-            <hr/>
-
-            <div class="demonstracao">
-                <div class="informacoes">
-                    <p>{descricao}</p>
-                    <details>
-                        <summary>Tecnologias usadas</summary>
-                        <ol>
-                            <li id="tecHtml">HTML5: {tecHtml}</li>
-                            <li id="tecCss">CSS3: {tecCss}</li>
-                            <li id="tecJavascript">JavaScript: {tecJavascript}</li>
-                        </ol>
-                    </details>
-                    <div class="link">
-                        <a href={linkVerCompleto} target="_blank">Ver completo</a>
-                        <a href={linkVerCodigo}>Ver código</a>
-                    </div>
-                </div>
-
-                <iframe src={linkIframe}></iframe>
-            </div>
-
-            <hr/>
+            {/* <hr/>
 
             <div class="demonstracao">
                 <div class="informacoes">
@@ -80,7 +92,8 @@ function Demonstracao() {
                 </div>
 
                 <iframe src={linkIframe}></iframe>
-            </div>
+            </div> */}
+
         </main>
     )
 }
