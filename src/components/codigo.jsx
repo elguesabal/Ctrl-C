479872ddd49cb7edf7f478a755b804f5e2d02626
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-24 18:37:01 -0300

    Add files via upload
    agora esta mais responsivo para celular

--- src/components/codigo.jsx
+++ src/components/codigo.jsx
@@ -60,7 +60,7 @@ function Codigo() {
                             <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
                         </svg>
                     </div>
-                    <textarea id="html" readonly="true">
+                    <textarea id="html" class="codigoTexto" readonly="true">
 
                     </textarea>
                 </div>
@@ -74,7 +74,7 @@ function Codigo() {
                             <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
                         </svg>
                     </div>
-                    <textarea id="css" readonly="true">
+                    <textarea id="css" class="codigoTexto" readonly="true">
 
                     </textarea>
                 </div>
@@ -88,7 +88,7 @@ function Codigo() {
                             <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
                         </svg>
                     </div>
-                    <textarea id="javascript" readonly="true">
+                    <textarea id="javascript" class="codigoTexto" readonly="true">
 
                     </textarea>
                 </div>
